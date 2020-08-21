14aaae5da569d11e36e4b498a9f50d5f8e0c9a18
SentinelDevv <51431408+JakenatorGames@users.noreply.github.com>
2020-08-21 00:07:29 -0700

    Create changehwid.php

---
+++ changehwid.php
@@ -0,0 +1,36 @@
+<?php
+$servername = "remotemysql.com";
+$name = "uJomDMgswL";
+$username = "uJomDMgswL";
+$password = "M7EZWGJ8LV";
+
+$key = $_GET["key"];
+$hwid = $_GET["hwid"];
+
+$conn = new mysqli($servername, $username, $password, $name);
+
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+function getUserIpAddr(){
+    if(!empty($_SERVER['HTTP_CLIENT_IP'])){
+        $ip = $_SERVER['HTTP_CLIENT_IP'];
+    }elseif(!empty($_SERVER['HTTP_X_FORWARDED_FOR'])){
+        $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+    }else{
+        $ip = $_SERVER['REMOTE_ADDR'];
+    }
+    return $ip;
+}
+
+$ips = getUserIpAddr();
+
+$sql = "UPDATE whitelistbot SET hwid = '$hwid', ip = '$ips' WHERE userkey = '$key'";
+
+if ($key and $hwid) {
+if ($conn->query($sql) === TRUE) {
+  echo "Hi Guys!";
+}
+
+?>
